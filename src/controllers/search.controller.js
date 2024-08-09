e97c8d6874663ff90a1ec708f90713d35711f3c7
dushyantsolanki <fullmoj70@gmail.com>
2024-08-09 16:35:33 +0530

    added a email field for search

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -2,16 +2,15 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { Students } from "../models/student.model.js";
 
 const searchStudent = asyncHandler(async (req, res) => {
-  const key = req.query.name;
-
-  console.log(key);
-  if (!key)
+  const search = req.query.search;
+  console.log(search);
+  if (!search)
     return res.status(400).json({ message: "search key is not found..." });
   try {
     const data = await Students.find({
       $or: [
-        { name: { $regex: req.query.name, $options: "i" } },
-        // { email: { $regexp: req.querey } },
+        { name: { $regex: search, $options: "i" } },
+        { email: { $regex: search, $options: "i" } },
       ],
     });
 
