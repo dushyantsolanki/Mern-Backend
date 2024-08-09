f421790a653531273c3d6834cd2a95143a1d5ab6
dushyantsolanki <fullmoj70@gmail.com>
2024-08-09 10:16:15 +0530

    added a search api route

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,14 +1,17 @@
 import { Router } from "express";
-import { fetchAllStudent } from "../controllers/listStudent.controller.js";
+import { fetchAllStudentByQuery } from "../controllers/listStudent.controller.js";
 import { idWiseStudent } from "../controllers/idWiseStudent.controller.js";
 import { postData } from "../controllers/postData.controller.js";
 import { updateData } from "../controllers/updateData.controller.js";
 import { deleteData } from "../controllers/deleteData.controller.js";
+import { searchStudent } from "../controllers/search.controller.js";
+
 
 const router = Router();
 
 // Define a route.
-router.route("/student").get(fetchAllStudent);
+router.route("/student").get(fetchAllStudentByQuery);
+router.route("/student/search").get(searchStudent);
 router.route("/student/:id").get(idWiseStudent);
 router.route("/student").post(postData);
 router.route("/student/:id").put(updateData);
