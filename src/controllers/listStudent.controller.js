351ec1c18c92f06edaa0a45a4ef1c45490e5d5c4
dushyantsolanki <fullmoj70@gmail.com>
2024-08-09 10:13:53 +0530

    update the code for pagiantion base on api

--- src/controllers/listStudent.controller.js
+++ src/controllers/listStudent.controller.js
@@ -1,13 +1,17 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Students } from "../models/student.model.js";
 
-const fetchAllStudent = asyncHandler(async (req, res) => {
+const fetchAllStudentByQuery = asyncHandler(async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 4;
   try {
-    const data = await Students.find();
+    const skip = (page - 1) * limit;
+    console.log("for Infinet scrolling  page : ", page);
+    const data = await Students.find().skip(skip).limit(limit).exec();
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ code: error.code, message: error.message });
   }
 });
 
-export { fetchAllStudent };
+export { fetchAllStudentByQuery };
